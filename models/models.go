ed006dbca68905353ae71c80344e50c1521516dc
Jijesh Mohan <jijeshmohan@gmail.com>
2014-04-22 22:50:09 +0530

    Init database

--- models/models.go
+++ models/models.go
@@ -1,1 +1,31 @@
 package models
+
+import (
+	"database/sql"
+
+	"github.com/coopernurse/gorp"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var (
+	orp *gorp.DbMap
+)
+
+type database struct {
+}
+
+func InitDb() (dbmap *gorp.DbMap, err error) {
+
+	db, err := sql.Open("sqlite3", "./db/database.sqlite")
+	if err != nil {
+		return nil, err
+	}
+
+	orp = &gorp.DbMap{Db: db, Dialect: gorp.SqliteDialect{}}
+
+	if err = orp.CreateTablesIfNotExists(); err != nil {
+		return
+	}
+	dbmap = orp
+	return
+}
