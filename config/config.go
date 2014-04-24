f206bb7cc03866b6a7fa17ee42866d9130ebc995
Jijesh Mohan <jijeshmohan@gmail.com>
2014-04-24 23:11:24 +0530

    config: load from environment variables

--- config/config.go
+++ config/config.go
@@ -1,11 +1,19 @@
 package config
 
-import "github.com/BurntSushi/toml"
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+
+	"github.com/BurntSushi/toml"
+)
 
 type Config struct {
-	Addr   string `toml:"bind_addr"`
-	DbType string `toml:"db_type"`
-	DbPath string `toml:"db_path"`
+	Addr   string `toml:"bind_addr" env:"MTG_BIND_ADDR"`
+	DbType string `toml:"db_type" env:"MTG_DB_TYPE"`
+	DbPath string `toml:"db_path" env:"MTG_DB_PATH"`
 }
 
 func New() *Config {
@@ -16,4 +24,33 @@ func New() *Config {
 func (c *Config) LoadFile(path string) error {
 	_, err := toml.DecodeFile(path, c)
 	return err
+}
+
+func (c *Config) LoadEnv() error {
+	value := reflect.Indirect(reflect.ValueOf(c))
+	typ := value.Type()
+	for i := 0; i < typ.NumField(); i++ {
+		field := typ.Field(i)
+
+		// Retrieve environment variable.
+		v := strings.TrimSpace(os.Getenv(field.Tag.Get("env")))
+		if v == "" {
+			continue
+		}
+
+		// Set the appropriate type.
+		switch field.Type.Kind() {
+		case reflect.Bool:
+			value.Field(i).SetBool(v != "0" && v != "false")
+		case reflect.Int:
+			newValue, err := strconv.ParseInt(v, 10, 0)
+			if err != nil {
+				return fmt.Errorf("Parse error: %s: %s", field.Tag.Get("env"), err)
+			}
+			value.Field(i).SetInt(newValue)
+		case reflect.String:
+			value.Field(i).SetString(v)
+		}
+	}
+	return nil
 }
--- config/config_test.go
+++ config/config_test.go
@@ -18,22 +18,33 @@ func TestConfig(t *testing.T) {
 	})
 
 	Convey("Load config file", t, func() {
+
 		Convey("should override default values", func() {
 			content := `db_path="testing.db"`
-			withTempFile(content, func(tempFile string) {
-				c := New()
-				So(c, ShouldNotBeNil)
+			withTempFile(content, func(c *Config, tempFile string) {
 				So(c.DbPath, ShouldContainSubstring, "development.db")
 				So(c.LoadFile(tempFile), ShouldBeNil)
 				So(c.DbPath, ShouldContainSubstring, "testing.db")
 			})
 		})
 
+		Convey("should throw error when the file is missing", func() {
+			c := New()
+			So(c.LoadFile("invalidfile"), ShouldNotBeNil)
+			So(c.DbPath, ShouldContainSubstring, "development.db")
+		})
+
+		Convey("should throw error when the file is in different format", func() {
+			content := `{db_path="testing.db"}`
+			withTempFile(content, func(c *Config, tempFile string) {
+				So(c.LoadFile(tempFile), ShouldNotBeNil)
+				So(c.DbPath, ShouldContainSubstring, "development.db")
+			})
+		})
+
 		Convey("should ignore invalid values present in the file", func() {
 			content := `db_path1="testing.db"`
-			withTempFile(content, func(tempFile string) {
-				c := New()
-				So(c, ShouldNotBeNil)
+			withTempFile(content, func(c *Config, tempFile string) {
 				So(c.DbPath, ShouldContainSubstring, "development.db")
 				So(c.LoadFile(tempFile), ShouldBeNil)
 				So(c.DbPath, ShouldContainSubstring, "development.db")
@@ -41,12 +52,30 @@ func TestConfig(t *testing.T) {
 		})
 	})
 
+	Convey("Load env", t, func() {
+
+		Convey("should override default values", func() {
+			os.Setenv("MTG_BIND_ADDR", ":4000")
+			os.Setenv("MTG_DB_TYPE", "mysql")
+			os.Setenv("MTG_DB_PATH", "another.db")
+
+			c := New()
+			So(c.LoadEnv(), ShouldBeNil)
+
+			So(c.Addr, ShouldEqual, ":4000")
+			So(c.DbPath, ShouldEqual, "another.db")
+			So(c.DbType, ShouldEqual, "mysql")
+		})
+
+	})
+
 }
 
-func withTempFile(content string, fn func(string)) {
+func withTempFile(content string, fn func(c *Config, tempFile string)) {
 	f, _ := ioutil.TempFile("", "")
 	f.WriteString(content)
 	f.Close()
 	defer os.Remove(f.Name())
-	fn(f.Name())
+	c := New()
+	fn(c, f.Name())
 }
