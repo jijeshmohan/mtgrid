c7e3456fc362faa07e19d6bcccc87ba67b966155
Jijesh Mohan <jijeshmohan@gmail.com>
2014-04-25 20:39:02 +0530

    added config in main

--- main.go
+++ main.go
@@ -1,23 +1,39 @@
 package main
 
 import (
+	"log"
+
 	"github.com/go-martini/martini"
-	_ "github.com/jijeshmohan/mtgrid/config"
+	"github.com/jijeshmohan/mtgrid/config"
 	_ "github.com/jijeshmohan/mtgrid/models"
 	"github.com/martini-contrib/render"
 )
 
 var (
 	m *martini.ClassicMartini
+	c *config.Config
 )
 
-func init() {
+func initServer() {
 	m = martini.Classic()
 	m.Use(render.Renderer(render.Options{
 		Layout: "layout",
 	}))
 }
 
-func main() {
+func startServer() {
 	m.Run()
+}
+
+func main() {
+	c := config.New()
+	if err := c.LoadFile("app.conf"); err != nil {
+		log.Fatalln("Error :", err)
+	}
+	if err := c.LoadEnv(); err != nil {
+		log.Fatalln("Error :", err)
+	}
+
+	initServer()
+	startServer()
 }
